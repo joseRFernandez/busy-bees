01904e09272af8064d1b28a422cca2c2311467bb
Jose_Fernandez <jfernand84@gmail.com>
2022-09-23 19:54:09 -0400

    [button]- additional tests

--- src/__tests__/BaseButton.spec.js
+++ src/__tests__/BaseButton.spec.js
@@ -5,43 +5,58 @@ import { mount } from "@vue/test-utils";
 import BaseButton from "../components/BaseButton.vue";
 describe("BaseButton", () => {
   describe("Able to render correctly", () => {
-    test("displays proper text", () => {
-      // mount() returns a wrapped Vue component we can interact with
+    it("displays proper text when no text props are passsed", () => {
+      const wrapper = mount(BaseButton);
+      expect(wrapper.text()).toBe("Button");
+    });
+    it("displays proper text", () => {
       const wrapper = mount(BaseButton, {
         props: {
           text: "Click Me",
         },
       });
-      // Assert the rendered text of the component
       expect(wrapper.text()).toContain("Click Me");
     });
-  });
-  test("displays proper styles when no additional class is passed", () => {
-    const wrapper = mount(BaseButton, {
-      props: {
-        text: "Default",
-      },
+    it("displays proper styles when no additional class is passed", () => {
+      const wrapper = mount(BaseButton, {
+        props: {
+          text: "Default",
+        },
+      });
+      expect(wrapper.attributes("class")).toBe("button");
     });
-    expect(wrapper.attributes("class")).toBe("button");
+
+    it("displays proper styles when 1 additional class is passed", () => {
+      const wrapper = mount(BaseButton, {
+        props: {
+          text: "Default",
+          propStyles: ["is-info"],
+        },
+      });
+      expect(wrapper.attributes("class")).toBe("button is-info");
+    });
+
+    it("displays proper styles when 2 additional classes are passed", () => {
+      const wrapper = mount(BaseButton, {
+        props: {
+          text: "Default",
+          propStyles: ["is-info hidden"],
+        },
+      });
+      expect(wrapper.attributes("class")).toBe("button is-info hidden");
+    });
   });
 
-  test("displays proper styles when 1 additional class is passed", () => {
-    const wrapper = mount(BaseButton, {
-      props: {
-        text: "Default",
-        propStyles: ["is-info"],
-      },
-    });
-    expect(wrapper.attributes("class")).toBe("button is-info");
-  });
+  describe("emits events to a parent properly", () => {
+    it("emits an event", async () => {
+      const wrapper = mount(BaseButton);
 
-  test("displays proper styles when 2 additional classes are passed", () => {
-    const wrapper = mount(BaseButton, {
-      props: {
-        text: "Default",
-        propStyles: ["is-info hidden"],
-      },
+      wrapper.vm.$emit("foo");
+      wrapper.vm.$emit("foo", 123);
+
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.emitted("foo")).toBeTruthy();
     });
-    expect(wrapper.attributes("class")).toBe("button is-info hidden");
   });
 });
